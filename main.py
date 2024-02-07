f8ec15bc9b1dd1c8448a1ce573bc210a6c600655
Le Hoang Dung <hoangdungle36@gmail.com>
2024-02-07 15:39:00 +0700

    + debug model 8

--- main.py
+++ main.py
@@ -22,10 +22,7 @@ train_loader,valid_loader,test_loader = get_data(args)
 
 # print(train_loader[0])
 batch = next(iter(train_loader))
-print(type(batch))
-print(batch.keys())
-element = batch[0]
-print(type(element))
+print(type(batch['labels']))
 # batch = next(train_loader)
 # test = batch[0]
 # print(type(test))
